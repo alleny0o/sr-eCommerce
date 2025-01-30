3e2e0ab44ecdb4672f0eadb5d9b4642212e7d005
Development <development@Allens-MacBook-Pro-4.local>
2025-01-30 13:16:42 -0500

    updated config for redis and s3

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,19 +1,26 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+// Copyright (c) 2023. Medusa Technologies GmbH. All rights reserved.
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+// load env
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
+// variables
+const PRODUCTION = process.env.NODE_ENV === "production";
+const BACKEND_URL = process.env.MEDUSA_BACKEND_URL || "http://localhost:9000";
+
+// export config
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
-    redisUrl: process.env.REDIS_URL,
+    ...(!PRODUCTION ? {} : { redisUrl: process.env.REDIS_URL }),
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
@@ -21,25 +28,62 @@ module.exports = defineConfig({
   },
   modules: [
     // redis modules
+    ...(PRODUCTION
+      ? [
+          {
+            resolve: "@medusajs/medusa/cache-redis",
+            options: {
+              redisUrl: process.env.REDIS_URL,
+            },
+          },
+          {
+            resolve: "@medusajs/medusa/event-bus-redis",
+            options: {
+              redisUrl: process.env.REDIS_URL,
+            },
+          },
+          {
+            resolve: "@medusajs/medusa/workflow-engine-redis",
+            options: {
+              redis: {
+                url: process.env.REDIS_URL,
+              },
+            },
+          },
+        ]
+      : []),
+    // aws s3
     {
-      resolve: "@medusajs/medusa/cache-redis",
+      resolve: "@medusajs/medusa/file",
       options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/event-bus-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/workflow-engine-redis",
-      options: {
-        redis: {
-          url: process.env.REDIS_URL,
-        },
+        providers: [
+          ...(PRODUCTION
+            ? [
+                {
+                  resolve: "@medusajs/medusa/file-s3",
+                  id: "s3",
+                  options: {
+                    file_url: process.env.S3_FILE_URL,
+                    access_key_id: process.env.S3_ACCESS_KEY_ID,
+                    secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+                    region: process.env.S3_REGION,
+                    bucket: process.env.S3_BUCKET,
+                    endpoint: process.env.S3_ENDPOINT,
+                  },
+                },
+              ]
+            : [
+                {
+                  resolve: "@medusajs/medusa/file-local",
+                  id: "local",
+                  options: {
+                    upload_dir: "static",
+                    backend_url: `${BACKEND_URL}/static`,
+                  },
+                },
+              ]),
+        ],
       },
     },
   ],
-})
+});

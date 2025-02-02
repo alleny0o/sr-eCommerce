8d342bb1d023ab683ada8d19dc77b2d7f876d89f
Development <development@MacBookPro.fios-router.home>
2025-02-01 22:03:30 -0500

    test 10

--- backend/src/lib/constants.ts
+++ backend/src/lib/constants.ts
@@ -5,4 +5,4 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 /**
  * Public URL for the backend
  */
-export const BACKEND_URL = process.env.BACKEND_PUBLIC_URL ?? process.env.RAILWAY_PUBLIC_DOMAIN_VALUE ?? 'http://localhost:9000'
+export const BACKEND_URL = process.env.MEDUSA_BACKEND_URL ?? process.env.RAILWAY_PUBLIC_DOMAIN_VALUE ?? 'http://localhost:9000'

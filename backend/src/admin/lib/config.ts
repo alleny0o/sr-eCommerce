81a8a86dd03d0054ea6d86783e1e40a314f9f6ab
Development <development@MacBookPro.fios-router.home>
2025-02-01 22:53:43 -0500

    fixed completely config.ts

--- backend/src/admin/lib/config.ts
+++ backend/src/admin/lib/config.ts
@@ -1,7 +1,13 @@
 import Medusa from "@medusajs/js-sdk"
 
+let MEDUSA_BACKEND_URL = "http://localhost:9000";
+
+if (import.meta.env.VITE_MEDUSA_BACKEND_URL) {
+  MEDUSA_BACKEND_URL = import.meta.env.VITE_MEDUSA_BACKEND_URL;
+};
+
 export const sdk = new Medusa({
-  baseUrl: process.env.NODE_ENV === "development" ? "http://localhost:9000" : "https://sr-ecommerce-production.up.railway.app",
+  baseUrl: MEDUSA_BACKEND_URL,
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
